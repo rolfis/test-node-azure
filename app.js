5164acd6090818d95d6f9d90dfcea940a9ffa2fc
Rolf Johansson <rojo@nocrew.org>
2022-08-16 13:44:59 +0200

    Add logger

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const pkg = require('./package.json');
 const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
+const logger = require('pino')();
 
+const port = process.env.PORT || 3000;
 const NODE_MAJOR_VERSION = process.versions.node.split('.')[0];
 const NODE_MINOR_VERSION = process.versions.node.split('.')[1];
 
@@ -28,6 +29,8 @@ app.enable('trust proxy');
 app.use("/assets", express.static(__dirname + '/public/assets'));
 
 app.get('/', async (request, response) => {
+    logger.info("Got request.");
+
     return response.send({
         success: true,
         version: pkg.version,
@@ -36,9 +39,9 @@ app.get('/', async (request, response) => {
     });
 });
 
-app.listen(port, () => console.log(`[Main] Application listening on port ${port}.`));
+app.listen(port, () => logger.info(`Application listening on port ${port}.`));
 
 process.on('uncaughtException', (err) => {
-    console.error('[Main] There was an uncaught error', err);
+    logger.error('There was an uncaught error', err);
     process.exit(1); //mandatory (as per the Node docs)
 });
